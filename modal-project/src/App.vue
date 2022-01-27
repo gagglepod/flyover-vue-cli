bf4d404bec7378aa1d4454c34b5f5dcb6593c0f4
Kyle Bondo <gagglepod@gmail.om>
2022-01-27 12:16:55 -0600

    Basic template, scripts, and methods.

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,13 +1,35 @@
 <template>
   <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal"><Modal :header="header" :text="text" theme="sale" @close="toggleModal" /></div>
+  <button @click.alt="toggleModal">open modal (alt)</button>
+  <h2>Input...</h2>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
     }
   }
 }
