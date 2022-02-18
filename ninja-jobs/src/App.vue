5b09cc40739690e9d8104be591f63863ec66ac3c
Kyle Bondo <gagglepod@gmail.om>
2022-02-18 15:38:33 -0600

    Redirects and 404 components.

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -4,8 +4,30 @@
     <router-link :to="{ name: 'About' }">About</router-link> | 
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go Back</button>
+  <button @click="forward">Go Forward</button>
+
   <router-view/>
 </template>
+
+<script>
+  export default {
+    methods: {
+      redirect() {
+        this.$router.push({ name: 'Home' })
+      }, 
+      back() {
+        this.$router.go(-1)
+      },
+      forward() {
+        this.$router.go(1)
+      }
+    }
+  }
+</script>
+
 
 <style>
 #app {
@@ -31,5 +53,12 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -3,6 +3,7 @@ import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Jobs from "../views/jobs/Jobs.vue";
 import JobDetails from "../views/jobs/JobDetails.vue";
+import NotFound from "../views/NotFound.vue";
 
 const routes = [
   {
@@ -25,6 +26,17 @@ const routes = [
     name: "JobDetails",
     component: JobDetails,
     props: true,
+  },
+  // redirct
+  {
+    path: "/all-jobs",
+    redirect: "/jobs",
+  },
+  // catch-all 404
+  {
+    path: "/:catchAll(.*)",
+    name: "Not Found",
+    component: NotFound,
   },
 ];
 
