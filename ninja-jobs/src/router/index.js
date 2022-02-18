6a50689587de6ae2b813ad5b66ec83fd852facfa
Kyle Bondo <gagglepod@gmail.om>
2022-02-18 10:26:55 -0600

    Created Routes.

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,25 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Jobs from "../views/jobs/Jobs.vue";
+import JobDetails from "../views/jobs/JobDetails.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/jobs",
+    name: "Jobs",
+    component: Jobs,
+  },
+  {
+    path: "/jobs/:id",
+    name: "JobDetails",
+    component: JobDetails,
+    props: true,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate doloribus, aperiam mollitia nulla minima exercitationem porro alias praesentium recusandae sequi nostrum explicabo sed adipisci itaque delectus consectetur, impedit eaque temporibus eius at, esse accusamus non. Illum molestiae suscipit nihil repellat ratione cupiditate earum, tenetur illo? Temporibus quod explicabo ratione incidunt quae! Odit earum odio autem, rem illo rerum harum quo reiciendis quaerat maiores. Nobis sapiente voluptas aliquam similique beatae nisi ad veritatis aspernatur sed, eos voluptatem harum quia, maxime explicabo ipsam accusamus veniam aut, incidunt atque repellendus laudantium. Eaque, maxime. Soluta magni odio beatae sequi quasi aliquam nulla nobis illum.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eveniet neque repellat consectetur minus ratione, corrupti vitae impedit voluptate, consequuntur, eligendi aliquid repudiandae! Cumque facilis, veritatis obcaecati quibusdam cum omnis iusto, inventore repellendus quisquam ad nulla, eligendi ullam provident placeat rem corporis laboriosam a! Corrupti consectetur aut placeat necessitatibus nisi odit nemo iure animi, accusantium optio ipsam dicta. Maiores placeat cupiditate incidunt assumenda quod iusto minus autem illum! Eligendi reiciendis, exercitationem nisi molestias facere illum veritatis dolorem, facilis, corporis suscipit amet voluptatem neque temporibus eum eaque debitis officia? Illo illum mollitia enim repellendus et assumenda maiores consequuntur qui maxime ipsa?</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
