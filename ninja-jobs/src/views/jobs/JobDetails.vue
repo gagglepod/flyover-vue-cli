92d4ccdbdc6e0a6f2a6af1d28e85c6a5dd46dfc1
Kyle Bondo <gagglepod@gmail.om>
2022-02-20 09:19:43 -0600

    JSON Server configured and working

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,16 +1,28 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if='job'>
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading Job Details...</p>
+  </div>
 </template>
 
 <script>
 export default {
     props: ['id'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
+    data() {
+      return {
+        job: null
+      }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs/' + this.id)
+        .then(res => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.message))
+    }
 }
 </script>
 
